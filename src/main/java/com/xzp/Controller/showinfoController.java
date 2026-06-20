fc01a8f03d64b9682f3acbb4ab862fa1c51c691b
xzpxzp123 <2748739022@qq.com>
2026-06-20 15:20:28 +0800

    Update showinfoController.java

--- src/main/java/com/xzp/Controller/showinfoController.java
+++ src/main/java/com/xzp/Controller/showinfoController.java
@@ -1,5 +1,6 @@
 package com.xzp.Controller;
 
+import com.xzp.Aop.InvokeLog;
 import com.xzp.Mapper.ScoreMapper;
 import com.xzp.Mapper.StudentMapper;
 import com.xzp.Mapper.TeacherMapper;
@@ -27,51 +28,52 @@ public class showinfoController {
     testPaperMapper testPaperMapper;
     @Autowired
     ScoreMapper scoreMapper;
+    @InvokeLog
     @RequestMapping("/Teacher/contro/show/getByid")
-    public ResopnseResult showTeacherInfo (@RequestHeader("token") String token)  {
+    public ResopnseResult showTeacherInfo (@RequestHeader("token") String token) throws Exception {
         ResopnseResult<Teacher> resopnseResult=new ResopnseResult<Teacher>();
         Claims claims= null;
-        try {
             claims = JWTUtils.parseJWT(token);
             String id=claims.getSubject();
             Teacher teacher=teacherMapper.getById(Integer.parseInt(id));
             resopnseResult.setCode(300);
             resopnseResult.setData(teacher);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("你未成功登录,无法访问教师端页面");
-        }
+//         catch (Exception e) {
+//            e.printStackTrace();
+//            throw new RuntimeException("你未成功登录,无法访问教师端页面");
+//        }
         return  resopnseResult;
     }
+    @InvokeLog
     @RequestMapping("/Student/contro/show/getByid" )
-    public ResopnseResult showStudentInfo(@RequestHeader("token") String token){
+    public ResopnseResult showStudentInfo(@RequestHeader("token") String token) throws Exception {
         ResopnseResult<Student> resopnseResult=new ResopnseResult<Student>();
-        try {
             Claims claims= JWTUtils.parseJWT(token);
             String id=claims.getSubject();
             Student student=studentMapper.findById(Integer.parseInt(id));
             resopnseResult.setCode(success_Stauts_Code);
            resopnseResult.setData(student);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("你未成功登录,无法访问学生端页面");
-        }
+//         catch (Exception e) {
+//            e.printStackTrace();
+//            throw new RuntimeException("你未成功登录,无法访问学生端页面");
+//        }
         return  resopnseResult;
     }
+    @InvokeLog
 @RequestMapping("/Teacher/contro/show/showtestPaperInfo")
-    public ResopnseResult showtestPaperInfo(String course){
+    public ResopnseResult showtestPaperInfo(@RequestParam String course){
         ResopnseResult<List<course_question>> resopnseResult=new ResopnseResult<>();
-       try{
            List<course_question> list=testPaperMapper.getAll(course);
            resopnseResult.setData(list);
            resopnseResult.setCode(success_Stauts_Code);
-       }
-       catch (Exception e){
-           e.printStackTrace();
-           throw new RuntimeException("Error");
-       }
+
+//       catch (Exception e){
+//           e.printStackTrace();
+//           throw new RuntimeException("Error");
+//       }
        return resopnseResult;
     }
+    @InvokeLog
     @RequestMapping("/Student/contro/show/showCourseinfo" )
     public ResopnseResult showCoursesinfo(@RequestHeader("token")String token){
     ResopnseResult<CourseList> resopnseResult=new ResopnseResult<>();
@@ -88,73 +90,73 @@ public class showinfoController {
         }
         return resopnseResult;
     }
+    @InvokeLog
     @RequestMapping("/Student/contro/show/showQuestions")
     public ResopnseResult showQuestions(@RequestParam String course){
         ResopnseResult<List<course_question>> resopnseResult=new ResopnseResult<>();
-        try{
+
         List<course_question> list=testPaperMapper.getAll(course);
-        for(course_question course_question:list){
-           course_question.setAnswer(null);
+        for(course_question course_question:list) {
+            course_question.setAnswer(null);
         }
         resopnseResult.setData(list);
         resopnseResult.setCode(success_Stauts_Code);
-        }
-        catch (Exception e){
-            e.printStackTrace();
-            throw new RuntimeException("考题加载失败");
-        }
+
+//        catch (Exception e){
+//            e.printStackTrace();
+//            throw new RuntimeException("考题加载失败");
+//        }
         return resopnseResult;
     }
+    @InvokeLog
     @RequestMapping("/Student/contro/show/reshowQuestions")
     public ResopnseResult reShowQuestions(@RequestParam String course){
         ResopnseResult<List<course_question>> resopnseResult=new ResopnseResult<>();
-        try{
             List<course_question> list=testPaperMapper.getAll(course);
             for(course_question course_question:list){
                 course_question.setAnswer(null);
             }
             resopnseResult.setData(list);
             resopnseResult.setCode(success_Stauts_Code);
-        }
-        catch (Exception e){
-            e.printStackTrace();
-            throw new RuntimeException("考题加载失败");
-        }
+
+//        catch (Exception e){
+//            e.printStackTrace();
+//            throw new RuntimeException("考题加载失败");
+//        }
         return resopnseResult;
     }
+    @InvokeLog
     @RequestMapping("/Teacher/contro/show/showPoints")
     public ResopnseResult showPoints(@RequestParam String course){
         ResopnseResult<List<exam_record>> resopnseResult=new ResopnseResult<>();
-        try{
             List<exam_record> list=scoreMapper.showAll(course);
             resopnseResult.setData(list);
             resopnseResult.setCode(success_Stauts_Code);
-        }
-        catch (Exception e){
-            e.printStackTrace();
-            throw new RuntimeException("加载失败");
-        }
+//        catch (Exception e){
+//            e.printStackTrace();
+//            throw new RuntimeException("加载失败");
+//        }
         return  resopnseResult;
     }
+    @InvokeLog
     @RequestMapping("/Teacher/contro/show/showBystu")
     public  ResopnseResult showoneBystu(@RequestParam String course,@RequestParam String stu_name){
         ResopnseResult<List<exam_record>> resopnseResult=new ResopnseResult<>();
-        try{
             List<exam_record> list=scoreMapper.showOne(course,stu_name);
             resopnseResult.setData(list);
             resopnseResult.setCode(success_Stauts_Code);
-        }
-        catch (Exception e){
-            e.printStackTrace();
-            throw new RuntimeException("Error");
-        }
+
+//        catch (Exception e){
+//            e.printStackTrace();
+//            throw new RuntimeException("Error");
+//        }
         return resopnseResult;
     }
+    @InvokeLog
     @RequestMapping("/Student/contro/grade/gradePaper")
-    public ResopnseResult gradePaper(@RequestBody ExamSubmmitDTO examSubmmitDTO,@RequestHeader("token")String token){
+    public ResopnseResult gradePaper(@RequestBody ExamSubmmitDTO examSubmmitDTO,@RequestHeader("token")String token) throws Exception {
         ResopnseResult<Integer> resopnseResult=new ResopnseResult<>();
         Integer Score=0;
-        try {
             String course=examSubmmitDTO.getCourse();
             List<course_question> list=testPaperMapper.getAll(course);
             for(course_question course_question:list){
@@ -175,12 +177,13 @@ public class showinfoController {
                 scoreMapper.InsertScore(course,stu_name,Integer.toString(Score),time,Integer.toString(Integer.parseInt(retake_flag)+1));
             }
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("Error");
-        }
+//         catch (Exception e) {
+//            e.printStackTrace();
+//            throw new RuntimeException("Error");
+//        }
             resopnseResult.setCode(success_Stauts_Code);
             resopnseResult.setData(Score);
             return resopnseResult;
     }
 }
+
